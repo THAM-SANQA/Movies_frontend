e0c2df08c14eef8034d9c66269658dd965e0599f
THAM-SANQA <ndabat020@gmail.com>
2024-02-28 20:47:52 +0200

    frontend

---
+++ src/services/movies.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+class MovieDataService {
+    getAll(page = 0) {
+        return axios.get(`https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies?page=${page}`)
+    }
+    get(id) {
+        return axios.get(`https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies/id/${id}`)
+    }
+    find(query, by = "title", page = 0, rating) {
+        return axios.get(
+            `https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
+        )
+    }
+    createReview(data) {
+        return axios.post("https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies/review", data)
+    }
+    updateReview(data) {
+        return axios.put("https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies/review", data)
+    }
+    deleteReview(id, userId) {
+        return axios.delete(
+            "https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies/review",
+            { data: { review_id: id, user_id: userId } }
+        )
+    }
+    getRatings() {
+        return axios.get("https://movie-reviews-backend-thamsanqa.onrender.com/api/v1/movies/ratings")
+    }
+}
+
+const movieDataService = new MovieDataService();
+
+export default movieDataService;
